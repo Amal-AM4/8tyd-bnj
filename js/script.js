0295441eb4c7b13f248e66639e2e4ef93dc32ddb
Amal A M <vectorhero007@gmail.com>
2025-07-20 07:15:10 +0530

    changes in the data

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document.querySelector('.save-btn').addEventListener('click', () => {
 BEGIN:VCARD
 VERSION:3.0
 FN:Askar Hussain
-TEL;TYPE=CELL:+91-9947234413
+TEL;TYPE=CELL:+91-8075299870
 EMAIL:nyxgstoreofficial@gmail.com
 URL:https://www.nyx-g.store/
 END:VCARD
@@ -56,9 +56,10 @@ const qr = new QRious({
 const contacts = [
     { type: '', icon: 'fa-solid fa-globe', data: 'nyx-g.store', href: 'https://www.nyx-g.store/' },
     { type: 'Email', icon: 'fa-envelope', data: '', href: 'mailto:nyxgstoreofficial@gmail.com' },
-    { type: 'Whatsapp', icon: 'fa-brands fa-whatsapp', data: 'Nyx-G Store', href: 'https://wa.me/+919947234413' },
-    { type: 'Instagram', icon: 'fa-brands fa-instagram', data: '@nyx_techno', href: 'https://www.instagram.com/nyx_techno' },
+    { type: 'Whatsapp', icon: 'fa-brands fa-whatsapp', data: 'Nyx-G Store', href: 'https://wa.me/+918075299870' },
+    { type: 'Instagram', icon: 'fa-brands fa-instagram', data: '@nyx.gstore', href: 'https://www.instagram.com/nyx.gstore/' },
     { type: 'LinkedIn', icon: 'fa-brands fa-linkedin', data: 'Askar Hussain', href: 'https://www.linkedin.com/in/askar-hussain-44316b31b/' },
+    { type: 'Facebook', icon: 'fa-brands fa-facebook', data: 'Askar Hussain', href: 'https://www.facebook.com/share/1D6XAji1Xr/' },
     { type: '', icon: 'fa-brands fa-x-twitter', data: 'NYX-G STORE', href: 'https://x.com/nyx_gstore' },
 ];
 
